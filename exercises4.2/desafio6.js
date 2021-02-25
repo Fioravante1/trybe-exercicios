8b45d1187b868e5fb8e5021e0c5a11e1dcbb88ec
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-02-25 20:48:40 -0300

    Inicio da resoluçao do sexto desafio

---
+++ exercises4.2/desafio6.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numerosPar = 0;
+let numerosImpar = 0;
+
+for(let index = 0; index < numbers.length; index++){
+    if(numbers[index] % 2 === 0){
+        
+    }
+}
