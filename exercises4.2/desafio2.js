ed653931ba48d1d3cbeadc7603ce416652092a6b
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-02-25 18:49:59 -0300

    Resoluçao do segundo  desafio

---
+++ exercises4.2/desafio2.js
@@ -0,0 +1,12 @@
+let soma = 0;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 0; index < numbers.length; index++){
+    soma += numbers[index]
+    
+}
+console.log(soma); //Resolução feita com ajuda do site devmedia link:https://www.devmedia.com.br/javascript-reduce-reduzindo-uma-colecao-em-um-unico-objeto/37981
+
+// Resposta: Fiz a soma dos números do array sem utilizar a função reduce(). Ao trocar a variavel de local o codigo funcionou. passei a variavel da lenha 3 para linha 1.
+
