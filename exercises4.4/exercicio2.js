9366e6af505fb16ede80f613a99295ab355acfc2
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-01 15:48:51 -0300

    Resoluçao do terceiro axercicio

--- exercises4.4/exercicio2.js
+++ exercises4.4/exercicio2.js
@@ -11,3 +11,5 @@ let player = {
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log('A jogadora'+' '+player.name+' '+ player.lastName+' '+'foi eleita a melhor do mundo em'+' '+player.bestInTheWorld+'!');
+
+console.log('A jogadora possui'+' '+player.medals.golden+' '+'medalhas de ouro'+' '+'e'+' '+player.medals.silver+' '+'medalhas de prata!')
