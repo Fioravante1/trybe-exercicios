237b8719a7fd829bfbd2857d7f8d9a607c04b19e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-24 18:48:41 -0300

    resoluçao do oitavo exer

---
+++ exercises7.2/exercicio8.js
@@ -0,0 +1,30 @@
+const allLessons = {
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+
+
+const estudanteTotal = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+
+console.log(estudanteTotal(allLessons));
+//Utilizai gabarito para entender a questão
