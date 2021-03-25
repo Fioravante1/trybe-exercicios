c880703a58fb567aaef62bf75227b41998fde2b0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-25 19:32:45 -0300

    cria documento para o segundo exer

--- exercises7.3/testeuni1.js
+++ exercises7.3/testeuni1.js
@@ -4,12 +4,16 @@ function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
   }
-
   return a + b;
 }
 
-sum(4, 6);
 
+assert.strictEqual(typeof sum, 'function');
 assert.strictEqual(sum(4,5), 9);
 assert.strictEqual(sum(0, 0), 0);
-assert.throws(() => { sum(4, '5') });
+assert.throws(() => { 
+  sum(4, '5') 
+});
+assert.throws(() => {
+  sum(4, '5');
+},/^Error: parameters must be numbers/);
