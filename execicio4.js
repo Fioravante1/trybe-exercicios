b5e936ea2b2173f339bcd3acbc7a3a47e59d8736
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-07 18:16:17 -0300

    Resolução do (Inico)quinto exercicio

--- execicio4.js
+++ execicio4.js
@@ -40,3 +40,5 @@ const filteredPeople = filterPeople(people);
 
 assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
 assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
+
+// Solução encontrada com a ajuda do gabarito.
